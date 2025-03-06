a2f6d24bcaf0576b44e7b7778e61bf487c57c183
dxmbssdizzy <juandcharry06@gmail.com>
2025-03-06 15:37:28 -0500

    Terminao

--- anim.js
+++ anim.js
@@ -4,40 +4,41 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "At the time", time: 15 },
-  { text: "The whisper of birds", time: 18 },
-  { text: "Lonely before the sun cried", time: 27 },
-  { text: "Fell from the sky", time: 32 },
-  { text: "Like water drops", time: 33 },
-  { text: "Where I'm now? I don't know why", time: 41 },
-  { text: "Nice butterflies in my hands", time: 47 },
-  { text: "Too much light for twilight", time: 54 },
-  { text: "In the mood for the flowers love", time: 59 },
-  { text: "That vision", time: 67 },
-  { text: "Really strong, blew my mind", time: 72 },
-  { text: "Silence Let me see what it was", time: 78 },
-  { text: "I only want to live in clouds", time: 83 },
-  { text: "Where I'm now? I don't know why", time: 91 },
-  { text: "Nice butterflies in my hands", time: 97 },
-  { text: "Too much light for twilight", time: 104 },
-  { text: "In the mood for the flowers love", time: 108 },
-  { text: "At the time", time: 144 },
-  { text: "The whisper of birds", time: 148 },
-  { text: "Lonely before the sun cried", time: 153 },
-  { text: "Fell from the sky", time: 158 },
-  { text: "Like water drops", time: 164 },
-  { text: "Where I'm now? I don't know why", time: 169 },
-  { text: "Nice butterflies in my hands", time: 176 },
-  { text: "Too much light for twilight", time: 183 },
-  { text: "In the mood for the flowers", time: 188 },
-  { text: "Love.", time: 140 },
+  { text: "Tengo algo que decirte", time: 19 },
+  { text: "Y no se como empezar", time: 20 },
+  { text: "A explicar lo que te quiero contar", time: 21 },
+  { text: " ", time: 29 },
+  { text: "No encuentro las palabras", time: 34 },
+  { text: "Ni versos, rima o prosa", time: 35 },
+  { text: "Quiza con una rosa", time: 36 },
+  { text: "Te lo pueda decir", time: 37 },
+  { text: " ", time: 44 },
+  { text: "No se ni desde cuando", time: 49 },
+  { text: "Llegaste de repente", time: 50 },
+  { text: "Mi corazon se puso a cantar", time: 51 },
+  { text: " ", time: 59 },
+  { text: "Te quiero solecito hermoso", time: 65 },
+  { text: "Y te entrego en esta rosa", time: 66 },
+  { text: "La vida que me pueda quedar", time: 67 },
+  { text: "Doy gracias al cielo", time: 73 },
+  { text: "Por haberte conocido", time: 74 },
+  { text: "Doy gracias al cielo", time: 79 },
+  { text: "Y le cuento a las estrellas", time: 80 },
+  { text: "Lo bonito que senti", time: 84 },
+  { text: "Cuando te conoci", time: 86 },
+  { text: "Sabes...", time: 90 },
+  { text: "¡Sabes una cosa!", time: 93 },
+  { text: "¡Que yo te quiero!", time: 98 },
+  { text: "¡Que sin ti me muero!", time: 110 },
+  { text: "¡Que si estas lejos!", time: 112 },
+  { text: " ", time: 119 },
 ];
 
 // Animar las letras
 function updateLyrics() {
   var time = Math.floor(audio.currentTime);
   var currentLine = lyricsData.find(
-    (line) => time >= line.time && time < line.time + 6
+    (line) => time >= line.time && time < line.time + 7
   );
 
   if (currentLine) {
